4f803bad4cdf68c55ad01a1545933488d5bd346e
KR Shanto <84588948+Shanto2005@users.noreply.github.com>
2022-01-14 17:45:12 +0600

    Added an fetch request functionality.
    This functionality will only create the user;

--- frontend/src/components/signup.rs
+++ frontend/src/components/signup.rs
@@ -1,12 +1,23 @@
+#![allow(dead_code, unused)]
 use reqwasm::http::FormData;
 use reqwasm::http::Request;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::spawn_local;
 use web_sys::HtmlInputElement;
 use weblog::console_log;
 use yew::prelude::*;
+
+// *********** User info send to the server ************* //
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserInfo {
+    username: String,
+    password: String,
+    nickname: String,
+    // TODO: Later I will add `img_url` frield
+}
 
 #[function_component(Signup)]
 pub fn signup() -> Html {
@@ -19,13 +30,13 @@ pub fn signup() -> Html {
         <>
             <div class="signup">
                 <label for="nickname" >{"Enter your nickname"}</label>
-                <input ref={nickname_ref.clone()} type="text" id="nickname" />
+                <input ref={nickname_ref.clone()} type="text" id="nickname" value="5" />
 
                 <label for="username" >{"Enter your username"}</label>
-                <input ref={username_ref.clone()} type="text" id="username" />
+                <input ref={username_ref.clone()} type="text" id="username" value="5"/>
 
                 <label for="password" >{"Enter your password"}</label>
-                <input ref={password_ref.clone()} type="text" id="password" />
+                <input ref={password_ref.clone()} type="text" id="password" value="5" />
 
                 <label for="img" >{"Enter your img"}</label>
                 <input ref={img_ref.clone()} type="file" id="img" accept="image/*"/>
@@ -35,35 +46,32 @@ pub fn signup() -> Html {
                     let nickname = nickname_ref.cast::<HtmlInputElement>().unwrap().value();
                     let username = username_ref.cast::<HtmlInputElement>().unwrap().value();
                     let password = password_ref.cast::<HtmlInputElement>().unwrap().value();
-                    let img = img_ref.cast::<HtmlInputElement>().unwrap().files().unwrap();
-                    let img_url =  Uuid::new_v4().to_string() + "----" +   &img.get(0).unwrap().name();
+                    // let img = img_ref.cast::<HtmlInputElement>().unwrap().files().unwrap();
+                    // let img_url =  Uuid::new_v4().to_string() + "----" +   &img.get(0).unwrap().name();
 
-                    console_log!("Nickname: ", nickname.clone());
-                    console_log!("Username: ", username.clone());
-                    console_log!("Password: ", password.clone());
-                    console_log!("Image url: ", img_url.clone());
-
-
-                    let form_data = FormData::new().unwrap();
-                    form_data.append_with_blob_and_filename("imgage", &img.get(0).unwrap(), &img_url).unwrap();
 
                     spawn_local(async move {
+                        let user_info = UserInfo {
+                            username,
+                            password,
+                            nickname,
+                        };
 
-                        let resp = Request::post("http://127.0.0.1:8000/upload-image/")
-                        .body(form_data)
-                        .send()
-                        .await
-                        .unwrap()
-                        .ok();
+                        let user_info_json = serde_json::to_string(&user_info).unwrap();
 
-                        console_log!(resp)
+                        let resp = Request::post("http://127.0.0.1:8000/auth/sign-up")
+                                .header("Content-Type", "application/json")
+                                .body(user_info_json)
+                                .send()
+                                .await
+                                .unwrap();
 
-                    })
-
+                        console_log!(format!("{:?}", resp));
+                    });
 
                 }}>{"Create account"}</button>
 
-            </div>
-        </>
+                </div>
+                </>
     }
 }
